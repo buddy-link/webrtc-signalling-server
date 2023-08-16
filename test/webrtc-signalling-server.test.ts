ea7463e9e6595d5db4a1848a053158da1aa61ea9
Louis Smith <15144861+smudger@users.noreply.github.com>
2023-08-16 19:33:53 +0100

    Initial commit

---
+++ test/webrtc-signalling-server.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as WebrtcSignallingServer from '../lib/webrtc-signalling-server-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/webrtc-signalling-server-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new WebrtcSignallingServer.WebrtcSignallingServerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
