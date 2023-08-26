107700c107c86e4400c754dd9c32b0689adc0233
Louis Smith <15144861+smudger@users.noreply.github.com>
2023-08-26 18:23:27 +0100

    Add tags to taggable resources

--- bin/webrtc-signalling-server.ts
+++ bin/webrtc-signalling-server.ts
@@ -2,8 +2,12 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { WebRtcSignallingServerStack } from '../lib/WebRtcSignallingServerStack';
+import {Aspects, Tag} from "aws-cdk-lib";
 
 const app = new cdk.App();
+
+Aspects.of(app).add(new Tag('project', 'webrtc-signalling-server'));
+
 new WebRtcSignallingServerStack(app, 'WebRtcSignallingServerStack');
 
 app.synth();
